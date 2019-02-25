e676c02c5f910b89518c8b6d55378f071668405a
Andrey232420 <andrey.2324.air@gmail.com>
2019-02-25 15:53:44 +0800

    First version

---
+++ src/tcp/Server.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcp;
+
+/**
+ *
+ * @author GT
+ */
+public class Server {
+
+    public Server() {
+        NewJFrame.jTextArea1.append("qqqqq");
+//        NewJFrame frame = new NewJFrame();
+//        System.out.println("ok");
+        String str = NewJFrame.jTextField2.getText();
+//        
+        System.out.println(str);
+//        
+//        frame.set(str);
+
+    }
+
+}
